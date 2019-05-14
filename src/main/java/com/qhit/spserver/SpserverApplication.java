b4d36ed232b32fde5e1757126daa040a7d719d9d
李继光 <2327913863@qq.com>
2019-05-14 09:52:01 +0800

    Initial commit

---
+++ src/main/java/com/qhit/spserver/SpserverApplication.java
@@ -0,0 +1,15 @@
+package com.qhit.spserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpserverApplication.class, args);
+    }
+
+}
